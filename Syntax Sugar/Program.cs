7816608711fa082f7d0c5d7932582929563fd1df
Brittany McClellan <brittanyhines36@yahoo.com>
2022-09-01 09:50:50 -0500

    saved work

---
+++ Syntax Sugar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Syntax_Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to mine";
+
+            //if (answer < 9)
+            //{
+                //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+                //response = answer + “greater than or equal to nine”;
+            //}
+
+        }
+    }
+}
